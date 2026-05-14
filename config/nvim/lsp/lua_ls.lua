5c2745db8a4923dcfd3a9377f848d7e9c703738f
214763524+ToaaMusic@users.noreply.github.com <214763524+ToaaMusic@users.noreply.github.com>
2026-05-14 23:11:32 +0800

    nvim: add ai cmp plugin, improve luals in nvim config folder, add nvim/README.md to note shortcuts

--- config/nvim/lsp/lua_ls.lua
+++ config/nvim/lsp/lua_ls.lua
@@ -1,29 +1,31 @@
 local M = {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = {
+		".emmyrc.json",
+		".luarc.json",
+		".luarc.jsonc",
+		".luacheckrc",
+		".stylua.toml",
+		"stylua.toml",
+		"selene.toml",
+		"selene.yml",
+		".git",
+	},
 
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  settings = {
-    Lua = {
-      workspace = {
-        library = {
-          vim.env.VIMRUNTIME,
-        }
-      }
-    }
-  },
-  root_markers = {
-    '.emmyrc.json',
-    '.luarc.json',
-    '.luarc.jsonc',
-    '.luacheckrc',
-    '.stylua.toml',
-    'stylua.toml',
-    'selene.toml',
-    'selene.yml',
-    '.git',
-  },
+  -- 注意：这里设置的是全局引用
+  -- 非全局不推荐在这里写而是使用 .luarc.json
+  -- 但是如果项目存在 .luarc.json, 这里将不会被加载
 
+	-- settings = {
+	-- 	Lua = {
+	-- 		workspace = {
+	-- 			library = {
+	-- 				vim.env.VIMRUNTIME,
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 }
-
 
 return M
