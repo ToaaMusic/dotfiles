bb6678e3934b9197441f3c070c919ad1c9cc88ba
214763524+ToaaMusic@users.noreply.github.com <214763524+ToaaMusic@users.noreply.github.com>
2026-05-20 22:13:34 +0800

    hypr: add floating rules

--- config/hypr/hyprland/rules.lua
+++ config/hypr/hyprland/rules.lua
@@ -1,6 +1,9 @@
+-- rules.lua
 -- WINDOW & WORKSPACE RULES
 -- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
 -- See https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+
+-- Example window rules that are useful
 
 -- Ignore maximize requests from all apps
 hl.window_rule({
@@ -23,6 +26,14 @@ hl.window_rule({
 	no_focus = true,
 })
 
+-- Layer rules also return a handle.
+-- local overlayLayerRule = hl.layer_rule({
+--   name  = "no-anim-overlay",
+--   match = { namespace = "^my-overlay$" },
+--   no_anim = true,
+-- })
+-- overlayLayerRule:set_enabled(false)
+
 -- Hyprland-run windowrule
 hl.window_rule({
 	name = "move-hyprland-run",
@@ -30,6 +41,8 @@ hl.window_rule({
 	move = "20 monitor_h-120",
 	float = true,
 })
+
+-- custom
 
 -- Borderless workspace w[tv1]
 hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
@@ -40,10 +53,10 @@ hl.window_rule({
 	rounding = 0,
 })
 
--- Workspace 1: scrolling layout, direction right
+-- Set workspace 1 scrolling layout
 hl.workspace_rule({ workspace = "1", layout = "scrolling" })
 
--- no_blur for specific window classes
+-- no_blur
 hl.window_rule({
 	name = "no-blur-ebrain",
 	match = { class = "EbrainAI.Linux" },
@@ -54,8 +67,44 @@ hl.window_rule({
 	match = { title = "ebrain-ai" },
 	no_blur = true,
 })
+-- hl.window_rule({
+-- 	name = "no-blur-cava",
+-- 	match = { title = "cava" },
+-- 	no_blur = true,
+-- })
+
+-- floating
+hl.window_rule({match = {title = "^(Copying — Dolphin)$" }, move = {40, 80}})
+hl.window_rule({ match = { class = "^(org.kde.dolphin)$" }, center = true })
+hl.window_rule({ match = { class = "^(org.kde.dolphin)$" }, float = true })
+hl.window_rule({ match = { class = "^(org.kde.dolphin)$" }, size = { "(monitor_w*0.65)", "(monitor_h*0.65)" } })
+hl.window_rule({ match = { title = "^(Open File)(.*)$" }, center = true })
+hl.window_rule({ match = { title = "^(Open File)(.*)$" }, float = true })
+hl.window_rule({ match = { title = "^(Select a File)(.*)$" }, center = true })
+hl.window_rule({ match = { title = "^(Select a File)(.*)$" }, float = true })
+hl.window_rule({ match = { title = "^(Open Folder)(.*)$" }, center = true })
+hl.window_rule({ match = { title = "^(Open Folder)(.*)$" }, float = true })
+hl.window_rule({ match = { title = "^(Save As)(.*)$" }, center = true })
+hl.window_rule({ match = { title = "^(Save As)(.*)$" }, float = true })
+hl.window_rule({ match = { title = "^(Library)(.*)$" }, center = true })
+hl.window_rule({ match = { title = "^(Library)(.*)$" }, float = true })
+hl.window_rule({ match = { title = "^(File Upload)(.*)$" }, center = true })
+hl.window_rule({ match = { title = "^(File Upload)(.*)$" }, float = true })
+hl.window_rule({ match = { title = "^(.*)(wants to save)$" }, center = true })
+hl.window_rule({ match = { title = "^(.*)(wants to save)$" }, float = true })
+hl.window_rule({ match = { title = "^(.*)(wants to open)$" }, center = true })
+hl.window_rule({ match = { title = "^(.*)(wants to open)$" }, float = true })
+hl.window_rule({ match = { class = "^(org.pulseaudio.pavucontrol)$" }, float = true })
+hl.window_rule({ match = { class = "^(org.pulseaudio.pavucontrol)$" }, center = true })
 hl.window_rule({
-	name = "no-blur-cava",
-	match = { title = "cava" },
-	no_blur = true,
+	match = { class = "^(org.pulseaudio.pavucontrol)$" },
+	size = { "(monitor_w*0.45)", "(monitor_h*0.45)" },
 })
+
+-- picture-in-picture
+hl.window_rule({match = {title = "^([Pp]icture[-\\s]?[Ii]n[-\\s]?[Pp]icture)(.*)$" }, float = true})
+hl.window_rule({match = {title = "^([Pp]icture[-\\s]?[Ii]n[-\\s]?[Pp]icture)(.*)$" }, keep_aspect_ratio = true})
+hl.window_rule({match = {title = "^([Pp]icture[-\\s]?[Ii]n[-\\s]?[Pp]icture)(.*)$" }, move = {"(monitor_w*0.73)", "(monitor_h*0.72)"} })
+hl.window_rule({match = {title = "^([Pp]icture[-\\s]?[Ii]n[-\\s]?[Pp]icture)(.*)$" }, size = {"(monitor_w*0.25)", "(monitor_h*0.25)"} })
+hl.window_rule({match = {title = "^([Pp]icture[-\\s]?[Ii]n[-\\s]?[Pp]icture)(.*)$" }, float = true})
+hl.window_rule({match = {title = "^([Pp]icture[-\\s]?[Ii]n[-\\s]?[Pp]icture)(.*)$" }, pin = true})
