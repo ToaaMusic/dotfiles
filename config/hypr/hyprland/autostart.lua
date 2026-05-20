b47bc8a20431517f8a79e6262b8a7fc999e16093
ToaaM <2408274738@qq.com>
2026-05-20 22:13:34 +0800

    hypr: add floating rules

--- config/hypr/hyprland/autostart.lua
+++ config/hypr/hyprland/autostart.lua
@@ -7,5 +7,6 @@ return function(V)
 		hl.exec_cmd("fcitx5 --replace -d")
 		hl.exec_cmd(V.browser)
 		hl.exec_cmd("$TOAAM_DOTFILES/scripts/start-hyprpaper.sh")
+    hl.exec_cmd("hyprctl setcursor Imouto 28")
 	end)
 end
