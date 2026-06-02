4cc7df095c0ac36cebad2e7003da13a6b6931121
214763524+ToaaMusic@users.noreply.github.com <214763524+ToaaMusic@users.noreply.github.com>
2026-06-02 18:31:23 +0800

    fix: static monitors...

--- config/hypr/hyprland/monitors.lua
+++ config/hypr/hyprland/monitors.lua
@@ -1,23 +1,40 @@
 -- Monitors
-local monitors = hl.get_monitors()
+-- local monitors = hl.get_monitors()
 
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
 
-for _, mon in ipairs(monitors) do
-	if mon.name == "eDP-1" then
-		hl.monitor({
-			output = mon.name,
-			mode = "preferred",
-			position = "0x0",
-			scale = 1.0,
-		})
-	elseif mon.name:find("HDMI") then
-		hl.monitor({
-			output = mon.name,
-			mode = "highrr",
-			position = "0x0",
-			scale = 1.0,
-			mirror = "eDP-1",
-		})
-	end
-end
+-- somehow didn't work on first time
+
+-- for _, mon in ipairs(monitors) do
+-- 	if mon.name == "eDP-1" then
+-- 		hl.monitor({
+-- 			output = mon.name,
+-- 			mode = "preferred",
+-- 			position = "0x0",
+-- 			scale = 1.0,
+-- 		})
+-- 	elseif mon.name:find("HDMI") then
+-- 		hl.monitor({
+-- 			output = mon.name,
+-- 			mode = "highrr",
+-- 			position = "0x0",
+-- 			scale = 1.0,
+-- 			mirror = "eDP-1",
+-- 		})
+-- 	end
+-- end
+
+hl.monitor({
+	output = "eDP-1",
+	mode = "preferred",
+	position = "0x0",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "HDMI-A-1",
+	mode = "highrr",
+	position = "0x0",
+	scale = 1,
+	mirror = "eDP-1",
+})
