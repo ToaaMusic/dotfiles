3518e08530681e9c4c2e427e147d07905609a8d8
ToaaM <2408274738@qq.com>
2026-08-18 15:59:26 +0800

    cleanup

--- config/hypr/hyprland/colors.lua
+++ config/hypr/hyprland/colors.lua
@@ -1,16 +1,12 @@
 local M = {}
 
--- color defaults (overridden by colors.g.lua if generated)
-M.active_border_color   = "rgba(c293a3ff)"
-M.inactive_border_color = "0xff382D2E"
-
 local colors_file = os.getenv("HOME") .. "/.config/hypr/hyprland/colors.g.lua"
 local colors_file_check = io.open(colors_file)
 if colors_file_check then
 	colors_file_check:close()
-	local scheme = dofile(colors_file)
-	M.active_border_color = scheme.brand or M.active_border_color
-	M.inactive_border_color = scheme.bg.border or M.inactive_border_color
+	local scheme            = dofile(colors_file)
+	M.active_border_color   = scheme.brand or "rgba(c293a3ff)"
+	M.inactive_border_color = scheme.bg.border or "0xff382D2E"
 end
 
 return M
